5d20be05e59eec3e4e11ead6d3fb9e5eedcc40bc
Jeff Abrahamson <jeff@purple.com>
2013-12-30 08:35:01 +0100

    Remove debugging.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ TO_BIN='pic-new
     pic_select.py
     pic-small
 '
-echo cp $TO_BIN "$HOME/bin/"
+/bin/cp $TO_BIN "$HOME/bin/"
 
 # This works with my dotfiles setup.
 cp pic-bash.sh $HOME/.dotfiles/bash/pictools
