8fcb4a91bce7eb24b646fd9cb2a5febf2694b16d
Jeff Abrahamson <jeff@purple.com>
2015-12-29 20:25:40 +0100

    Add additional ufraw batch functions.
    Note that shadow directories are (soon) probably moribund.

--- pic-bash.sh
+++ pic-bash.sh
@@ -12,7 +12,19 @@ pic-dates() {
 }
 pic-select() { pic_select.py $1; }
 
-ub() {
+# ufraw with favorite options.
+ufr() {
+    infile="$1.cr2"
+    outfile="$1-2.jpg"
+    if [ ! -r "$infile" -o -e "$outfile" ]; then
+	echo "Existence check failed."
+    else
+	ufraw --create-id=also "$infile" --output="$outfile"
+    fi
+}
+
+# ufraw-batch with favorite options.
+ufb() {
     infile="$1.cr2"
     outfile="$1-2.jpg"
     if [ ! -r "$infile" -o -e "$outfile" ]; then
@@ -31,6 +43,10 @@ ub() {
 # pointing to the source images.
 #
 # This will break if image file names contain spaces.
+#
+# This was useful when I was using a machine that was too weak to
+# display images quickly at full size.  It is probably of decreasing
+# interest.
 pic-make-shadow() {
     _source="$1"
     if [ X = "X$_source" -a -e orig ]; then
@@ -60,6 +76,8 @@ pic-make-shadow() {
     fi
     # Use the checksum flag in order to avoid updating time with
     # --update and then copying both ways.
+    echo "Copying text files to orig/"
     rsync -v --checksum --update --exclude='*~' *txt* orig/
+    echo "Copying text files from orig/"
     rsync -v --checksum --update --exclude='*~' orig/*txt* ./
 }
